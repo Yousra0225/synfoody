77f11a5a5ad96d33342c2cf479de1a69601c4ed2
Yousra0225 <chbibyousra02@gmail.com>
2025-07-29 16:33:31 +0200

    feat(recipe-controller): update with data from database

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,13 +11,15 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class RecipeController extends AbstractController {
     #[Route('/recipe', name: 'recipe.index')]
-    public function index(Request $request) : Response {
+    public function index(Request $request, recipeRepository $repository) : Response {
         //return new Response('Salam, bienvenue sur la page des recettes ');
-        return $this->render('recipe/index.html.twig');
+       $recipes =  $repository->findAll();
+       return $this->render('recipe/index.html.twig', ['recipes' => $recipes]);
     }
 
     #[Route('/recipe/{slug}-{id}', name: 'recipe.show')]
-    public function show(Request $request, String $slug, int $id): Response{
+    public function show(Request $request, String $slug, int $id, RecipeRepository $repository): Response{
+        $recipe = $repository->findOneBy([$slug => 'slug']);
         return $this->render(( 'recipe/show.html.twig'), ['slug' => $slug, 'id' => $id]);
     }
 
