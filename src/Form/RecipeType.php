10e57be6cb0ee44c496f774d717d91afc764ad58
Yousra0225 <chbibyousra02@gmail.com>
2025-08-01 14:38:53 +0200

    feat(recipe-type): add listener for auto generation of slug

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -4,9 +4,12 @@ namespace App\Form;
 
 use App\Entity\Recipe;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class   RecipeType extends AbstractType
 {
@@ -20,7 +23,19 @@ class   RecipeType extends AbstractType
             ->add('ingredients')
             ->add('save', SubmitType::class, [
                 'label' => 'Enregistrer'])
+            ->addEventListener( formEvents::PRE_SUBMIT, $this->autoSlug(...))
+            ->addEventListener(FormEvents::POST_SUBMIT, [$this->autoDate(...))
         ;
+    }
+
+    public function autoSlug(PreSubmitEvent $event): void
+    {
+        $data = $event->getData();
+        if(empty($data['slug'])) {
+            $slugger = new AsciiSlugger();
+            $data['slug']= $slugger->slug($data['title'])->lower()->toString();
+        }
+        $event->setData($data);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
